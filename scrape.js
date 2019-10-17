3a52df204339b536c9993919c619086f5e95efe9
averywald <averywald.dev@gmail.com>
2019-10-17 13:54:36 -0500

    refactored GET requests in scrape.js

--- scrape.js
+++ scrape.js
@@ -2,30 +2,67 @@
 // scrape.js
 // avery wald
 
+// API stuff
+let root = 'http://augur.osshealth.io:5000/api/unstable/repo-groups/';
+let endpoints = [
+    { criteria: 'repo_group_id', extension: '/repos/' },
+    { criteria: 'repo_id', extension: '/code-changes/' },
+    { criteria: 'repo_id', extension: '/top-committers/' }
+];
+
+// send AJAX GET request
+// accepts:
+    // url to send GET request to
+    // index of endpoint criteria to parse for
+// returns:
+    // GET response with updated URL for the next request down the pipe
+function request(url, index) {
+    // initialize object container
+    var obj = new Object();
+    // GET request
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        async: false
+    }).done((data) => {
+        // assign GET response data to object field
+        obj.data = data;
+        // parse GET response entry objects
+        $.each(data, (k) => {
+            // append new field to each containing updated URL 
+            data[k].url = url 
+                + data[k][index.criteria] 
+                + index.extension;
+        });
+    });
+    return obj;
+}
+
+// once page is loaded
 $(document).ready(() => {
 
-    // send base GET request to augur REST API
-    $.ajax({
-        dataType: 'json',
-        url: 'http://augur.osshealth.io:5000/api/unstable/repo-groups',
-        method: 'GET',
-    }).done((data) => {
+        // API root GET request
+        $.when(request(root, endpoints[0])).done((response) => {
 
-        // iterate through the objects in JSON response
-        $.each(data, (key, value) => {
+            // ready to request repos
+            console.log(response);
 
-            // log the data for dev purposes
-            console.log(key, value['rg_name']);
+            // request code changes and repos
+                // double when
+            $.when(request(response.data[0].url, endpoints[1])).done((response) => {
 
-            // add data point group to DOM
-            $('body').append(
-                `<div id="${key}" class="dataPoint">
-                    <h3>${value['rg_name']}</h3>
-                </div>`
-            );
+                console.log(response);
 
+                $.when(request(response.data[0].url, endpoints[2])).done((response) => {
+
+                    console.log(response);
+
+
+
+                });
+
+            });
+            
         });
-
-    });
 
 });
