4d7a52698ea5c790dd8ae80b90119dbdfec87ca8
averywald <averywald.dev@gmail.com>
2019-10-18 09:56:55 -0500

    commit pctgs

--- scrape.js
+++ scrape.js
@@ -39,36 +39,56 @@ function request(url, index) {
     return obj;
 }
 
-function domify(data) {
-
-    data.foreach(e => {
-        $(body).append(
-            
-        );
-    });
-}
+// function domify(data) {
+//     for (let item in data) {
+//         $('body').append(`<h3>${item}</h3>`);
+//     }
+// }
 
 // once page is loaded
 $(document).ready(() => {
 
-    const i = 0;
+    var i = 0;
+    var commits = 0;
 
         // API root GET request
         $.when(request(root, endpoints[0])).done((response) => {
-            console.log(response.data);
+            // console.log(response.data);
+            // domify(response.data);
 
             $.when(request(response.data[i].url, endpoints[1])).done((response) => {
-                console.log(response.data);
+                // console.log(response.data);
 
                 $.when(request(response.data[i].url, endpoints[3])).done((response) => {
-                    console.log(response.data);
+                    console.log('code changes: ', response.data);           
+                    
+                    // get total commits per repo
+                    let arr = response.data;
+                    arr.forEach(element => {
+                        commits += element['commit_count'];
+                    });
+                    console.log('total commits: ', commits);
+                    
                 });
             });
             $.when(request(response.data[i].url, endpoints[2])).done((response) => {
-                console.log(response.data);
+                // console.log(response.data);            
 
                 $.when(request(response.data[i].url, endpoints[3])).done((response) => {
-                    console.log(response.data);
+                    // console.log(response.data);
+
+                    // get the top committers and their percentage of total commits
+                    let arr = response.data;
+                    arr.forEach(e => {
+                        let str = 
+                            'commit pctg: ' +
+                            e['email'] + ' ' +
+                            'commits: ' +
+                            e['commits'] + ' ' +
+                            (e['commits'] / commits);
+                        $('body').append(`<h3>${str}</h3>`);
+                    });
+                    // domify(response.data);
                 });
             });
         });
